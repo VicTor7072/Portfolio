a2b6635bfc08db17bbae2761a2bab75cc523ec31
Çetin <cetin200012@gmail.com>
2024-05-15 09:16:47 +0300

    changed photo

--- egemen2/src/components/Hero.tsx
+++ egemen2/src/components/Hero.tsx
@@ -1,28 +1,28 @@
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowDown } from 'react-icons/fa';
 
 const Hero = () => {
-  return (
-    <section className="h-[calc(100vh-60px)] min-h-[715px] flex flex-col justify-between py-5">
-      <div></div>
+	return (
+		<section className="flex h-[calc(100vh-60px)] min-h-[715px] flex-col justify-between py-5">
+			<div></div>
 
-      <div className="flex flex-col sm:flex-row w-full justify-between items-center gap-5">
-        <div className="text-4xl sm:text-6xl lg:text-8xl font-medium flex flex-col gap-y-5 lg:gap-y-10 tracking-tight sm:text-nowrap self-start sm:self-auto">
-          <p>Hi. I am a</p>
-          <p className="font-playfair text-4xl sm:text-5xl lg:text-7xl italic">
-            computer programmer
-          </p>
-          <p>based in İzmir</p>
-        </div>
+			<div className="flex w-full flex-col items-center justify-between gap-5 sm:flex-row">
+				<div className="flex flex-col gap-y-5 self-start text-4xl font-medium tracking-tight sm:self-auto sm:text-nowrap sm:text-6xl lg:gap-y-10 lg:text-8xl">
+					<p>Hi. I am a</p>
+					<p className="font-playfair text-4xl italic sm:text-5xl lg:text-7xl">
+						computer programmer
+					</p>
+					<p>based in İzmir</p>
+				</div>
 
-        <img src="imgs/pp9.png" alt="" className="w-96" />
-      </div>
+				<img src="imgs/pp9.jpg" alt="" className="w-96" />
+			</div>
 
-      <a href="" className="flex items-center gap-x-5">
-        <FaArrowDown className="text-3xl lg:text-5xl" />
-        <p className="text-lg font-semibold">scroll to continue</p>
-      </a>
-    </section>
-  );
+			<a href="" className="flex items-center gap-x-5">
+				<FaArrowDown className="text-3xl lg:text-5xl" />
+				<p className="text-lg font-semibold">scroll to continue</p>
+			</a>
+		</section>
+	);
 };
 
 export default Hero;
